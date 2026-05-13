07ce231b3c4213ad78e0e8efccd29c292cf6dfa0
Catalina Ramírez <blackandromeda.t@gmail.com>
2026-05-13 12:15:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const menuBtn = document.getElementById("menu-btn");
-const navMenu = document.getElementById("nav-menu");
 
 menuBtn.addEventListener("click", () => {
 
-    navMenu.classList.toggle("active");
+    alert("Menu functionality coming soon.");
 
 });
